31dd350892cf7cb7bc9f9016eeb48c37dcedd4c9
varunbajaj02 <varunbajaj3690@gmail.com>
2025-04-09 08:36:59 +0530

    new code

---
+++ add.java
@@ -0,0 +1,9 @@
+package java;
+
+public class add {
+        public static void main(String[] args){
+            int a=10,b=5;
+            int sum=a+b;
+            System.out.println("addition: " +sum);
+        }
+}

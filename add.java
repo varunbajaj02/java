e0c9ba247d471bd41b2fbd53ec1c72a94413f9d1
varunbajaj02 <varunbajaj3690@gmail.com>
2025-04-09 14:57:37 +0530

    Save th file

--- add.java
+++ add.java
@@ -2,7 +2,7 @@
 public class add {
         public static void main(String[] args){
             int a=10,b=5;
-            int sum=a+b;
-            System.out.println("add: " +sum);
+            int sub=a-b;
+            System.out.println("add: " +sub);
         }
 }

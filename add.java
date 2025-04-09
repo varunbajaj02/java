a020791bae153a3bbc2f1120e3e71f28b980cb26
varunbajaj02 <varunbajaj3690@gmail.com>
2025-04-09 14:45:52 +0530

    add

--- add.java
+++ add.java
@@ -2,7 +2,7 @@
 public class add {
         public static void main(String[] args){
             int a=10,b=5;
-            int sub=a-b;
-            System.out.println("subtraction: " +sub);
+            int sum=a+b;
+            System.out.println("add: " +sum);
         }
 }
